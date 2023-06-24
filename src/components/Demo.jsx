b98cf1d2414e2e731d546e78d06d991ebb5f6749
AbhishekBhure <abhiab7411@gmail.com>
2023-06-24 00:57:10 -0700

    started with demo, rapidApi, redux store

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      console.log(newArticle);
+    }
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* serch */}
+      <div className=" flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="linkIcon"
+            className="absolute  my-2 left-0 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter a Url"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            {" "}
+            ↵{" "}
+          </button>
+        </form>
+
+        {/* Browse URL History */}
+      </div>
+
+      {/* Display Result */}
+    </section>
+  );
 };
 
 export default Demo;
